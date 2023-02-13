0eefbec40918c7035870445b143b84f26eacbda6
SALAH AHMED <mersal1722002@gmail.com>
2023-02-13 06:37:01 +0200

    midyfied

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function(){
+    return "Homepage";
+})->name('mainpage');
+
+Route::get('about', function(){
+    return "About Us Page";
+});
+
+Route::get('services', function(){
+    return "Services";
+});
+
+Route::get('news/{id}', function($id){
+    return 'News #' . $id;
+});
+
+Route::get('users/{name?}', function($name = null){
+        return 'Hello ' . $name;
+});
+
+Route::prefix('admin')->group(function(){
+    Route::get('/home', function(){});
+    Route::get('/posts', function(){});
+    Route::get('/products', function(){});
+    Route::get('/comments', function(){});
+});
